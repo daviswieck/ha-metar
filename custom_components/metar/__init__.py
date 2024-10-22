c0993817215528818a312973d7cd4ba4244a5ed5
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-10-21 20:49:04 -0500

    Create __init__.py

---
+++ custom_components/metar/__init__.py
@@ -0,0 +1,17 @@
+"""The METAR Sensor integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the METAR sensor from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle unloading of a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
