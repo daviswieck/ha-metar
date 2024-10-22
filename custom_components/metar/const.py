a28e48e794a36f3200a100ee006f0448ca61d0f9
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2024-10-21 21:17:25 -0500

    Update const.py

--- custom_components/metar/const.py
+++ custom_components/metar/const.py
@@ -1,4 +1,5 @@
 """Constants for the METAR Sensor integration."""
 
-DOMAIN = "metar"
+DEFAULT_UPDATE_INTERVAL = 15  # Default interval in minutes
 BASE_URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations/{station}.TXT"
+DOMAIN = "metar"
