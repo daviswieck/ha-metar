9d1143e2b9966349efb20ba56a21e726634e8465
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2025-06-26 16:32:36 -0500

    Update __init__.py

--- custom_components/metar/__init__.py
+++ custom_components/metar/__init__.py
@@ -7,7 +7,7 @@ from .const import DOMAIN
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the METAR sensor from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
